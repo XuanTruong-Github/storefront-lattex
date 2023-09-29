6bb6e0dcf54d55651818254cda2278f41a0a3791
XuanTruong-Github <lexuantruong.contacts@gmail.com>
2023-09-29 14:27:33 +0700

    updated

--- src/themes/default/app/products/components/product-detail/price.tsx
+++ src/themes/default/app/products/components/product-detail/price.tsx
@@ -1,18 +1,43 @@
+'use client';
 import { cn } from '@/core/lib/utils';
 import helpers from '@/core/utils/helpers';
+import type { Product } from '@/core/modules/product/type';
+import { useMemo } from 'react';
+
 type Props = {
-  product: any;
+  product: Product;
   className?: string;
 };
 export default function Price({ product, className }: Props) {
+  const specialPrice = useMemo(() => {
+    if (product.hasOwnProperty('firstVariant')) {
+      return helpers.formatMoney(
+        product.firstVariant?.original_final_price ||
+          product.firstVariant?.price
+      );
+    } else
+      return helpers.formatMoney(
+        product?.original_final_price || product?.price
+      );
+  }, [product]);
+  const originalPrice = useMemo(() => {
+    if (
+      product.firstVariant?.original_price &&
+      product.firstVariant.original_price > product.firstVariant.price &&
+      product.firstVariant.original_price >
+        product.firstVariant.original_final_price
+    ) {
+      return helpers.formatMoney(product.firstVariant.original_price);
+    } else return null;
+  }, [product]);
   return (
     <div className={cn('flex items-start gap-x-2', className)}>
-      <h2 className='text-destructive'>
-        {helpers.formatMoney(product.original_final_price)}
-      </h2>
-      <span className='text-xl font-medium text-gray-400 line-through'>
-        {helpers.formatMoney(product.price)}
-      </span>
+      <h2 className='text-destructive'>{specialPrice}</h2>
+      {originalPrice && (
+        <span className='text-xl font-medium text-gray-400 line-through'>
+          {originalPrice}
+        </span>
+      )}
     </div>
   );
 }
