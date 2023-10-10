1da5c45a21f24dc1f31180a1f84b935a66ddbc7d
XuanTruong-Github <lexuantruong.contacts@gmail.com>
2023-10-10 14:31:00 +0700

    updated

--- src/themes/default/app/products/components/product-detail/gallery.tsx
+++ src/themes/default/app/products/components/product-detail/gallery.tsx
@@ -1,13 +1,13 @@
 'use client';
-import Slider from 'react-slick';
 import Image from 'next/image';
-import { Dialog, DialogContent } from 'ui/dialog';
 import { AspectRatio } from 'ui/aspect-ratio';
-import { useMemo, useState } from 'react';
-import type { Product } from '@/core/modules/product/type';
+import { Pagination, FreeMode, Thumbs } from 'swiper/modules';
 import helpers from '@/core/utils/helpers';
-import { cn } from '@/core/lib/utils';
 import useResponsive from '@/core/hooks/useResponsive';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useMemo, useState } from 'react';
+import { cn } from '@/core/lib/utils';
+import type { Product } from '@/core/modules/product/type';
 type Props = {
   product: Product;
   className?: string;
@@ -15,12 +15,7 @@ type Props = {
 
 export function Gallery({ product, className }: Props) {
   const { isDesktop } = useResponsive();
-  const [nav1, setNav1] = useState<any>(null);
-  const [nav2, setNav2] = useState<any>(null);
-  const [showPreviewImage, setShowPreviewImage] = useState(false);
-  const [imageSelected, setImageSelected] = useState<string | null>(null);
-  const activeStyle =
-    '[&>.slick-list>.slick-track>.slick-current]:border [&>.slick-list>.slick-track>.slick-current]:border-primary [&>.slick-list>.slick-track>.slick-current]:rounded-md';
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
   const images = useMemo<string[]>(
     () =>
       product.media_gallery.map(({ image }) =>
@@ -28,86 +23,66 @@ export function Gallery({ product, className }: Props) {
       ),
     [product.media_gallery]
   );
-  const onPreviewImage = (image: string) => {
-    setImageSelected(image);
-    setShowPreviewImage(true);
-  };
 
   return (
     <div className={className}>
-      <Slider
-        ref={(ref) => setNav1(ref)}
-        asNavFor={nav2}
-        dots={false}
-        arrows={false}
-        speed={700}
-        slidesToShow={1}
-        slidesToScroll={1}
-        className='mb-2'
-        focusOnSelect
-        infinite
-        fade
+      <Swiper
+        slidesPerView={1}
+        pagination={{
+          type: 'fraction',
+        }}
+        thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+        modules={[Pagination, FreeMode, Thumbs]}
+        className={cn(
+          'mb-2',
+          '[&>.swiper-pagination]:rounded-3xl',
+          '[&>.swiper-pagination]:bg-secondary',
+          '[&>.swiper-pagination]:text-secondary-foreground',
+          '[&>.swiper-pagination]:text-sm',
+          '[&>.swiper-pagination]:px-2',
+          '[&>.swiper-pagination]:py-[2px]',
+          '[&>.swiper-pagination]:w-fit',
+          '[&>.swiper-pagination]:right-2',
+          '[&>.swiper-pagination]:left-[unset]'
+        )}
       >
         {images.map((image: string, key: number) => (
-          <AspectRatio key={key} ratio={1}>
-            <Image
-              src={image}
-              alt={product.name}
-              priority
-              sizes='(min-width: 375px) 100%'
-              className='md:rounded-lg'
-              fill
-              onClick={() => onPreviewImage(image)}
-            />
-          </AspectRatio>
-        ))}
-      </Slider>
-      {images.length > 1 && (
-        <Slider
-          ref={(ref) => setNav2(ref)}
-          asNavFor={nav1}
-          dots={false}
-          arrows={isDesktop}
-          infinite={false}
-          slidesToShow={isDesktop ? 5 : 4}
-          slidesToScroll={1}
-          className={cn(
-            'px-2 [&>.slick-list>.slick-track>.slick-slide]:!p-1',
-            activeStyle
-          )}
-          focusOnSelect
-        >
-          {images.map((image: string, key: number) => (
-            <AspectRatio key={key} ratio={1}>
+          <SwiperSlide key={key}>
+            <AspectRatio ratio={1}>
               <Image
                 src={image}
                 alt={product.name}
+                priority
                 sizes='(min-width: 375px) 100%'
-                className='md:rounded-md'
                 fill
               />
             </AspectRatio>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className='px-1'>
+        <Swiper
+          spaceBetween={8}
+          slidesPerView={isDesktop ? 5 : 4}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Thumbs]}
+          onSwiper={setThumbsSwiper}
+        >
+          {images.map((image: string, key: number) => (
+            <SwiperSlide key={key}>
+              <AspectRatio ratio={1} className={cn()}>
+                <Image
+                  src={image}
+                  alt={product.name}
+                  sizes='(min-width: 375px) 100%'
+                  fill
+                />
+              </AspectRatio>
+            </SwiperSlide>
           ))}
-        </Slider>
-      )}
-      {!!imageSelected && (
-        <Dialog open={showPreviewImage} onOpenChange={setShowPreviewImage}>
-          <DialogContent
-            className={cn(
-              'max-h-[90%] max-w-[90%] overflow-y-auto rounded-lg p-0 sm:!w-fit ',
-              '[&>button]:right-2 [&>button]:top-2 [&>button]:border [&>button]:bg-white [&>button]:p-2 focus:[&>button]:!ring-0 focus:[&>button]:!ring-offset-0'
-            )}
-          >
-            <Image
-              src={imageSelected}
-              alt='Review'
-              width={1000}
-              height={1000}
-              className='w-full object-contain sm:w-auto'
-            />
-          </DialogContent>
-        </Dialog>
-      )}
+        </Swiper>
+      </div>
     </div>
   );
 }
